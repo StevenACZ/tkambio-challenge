b32447d7b236e93f15e7cbb7e55831096cbdeac3
stevenacz <scoailazaa@gmail.com>
2021-04-21 13:45:38 -0500

    improved

---
+++ src/components/ui/Button.vue
@@ -0,0 +1,26 @@
+<template>
+  <button class="btn">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    text: String,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.btn {
+  background: none;
+  border: 1px solid white;
+  border-radius: 50px;
+  font-size: 14px;
+  font-weight: normal;
+  line-height: 15px;
+  color: white;
+  padding: 16px 30px;
+  cursor: pointer;
+}
+</style>
