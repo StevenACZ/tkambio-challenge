79f2e6d7e8005fd5f7b428d914f419a99ed804f1
stevenacz <scoailazaa@gmail.com>
2021-04-21 13:22:54 -0500

    improved

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,33 @@
 <template>
-  <div class="home"></div>
+  <div class="hero">
+    <Header />
+    <main class="home">
+      Main
+    </main>
+    <Footer />
+  </div>
 </template>
 
 <script>
+// Components
+import Header from '../components/layout/Header';
+import Footer from '../components/layout/Footer';
+
 export default {
   name: 'Home',
+  components: {
+    Header,
+    Footer,
+  },
 };
 </script>
+
+<style scoped>
+.hero {
+  background: red;
+}
+
+.home {
+  color: red;
+}
+</style>
